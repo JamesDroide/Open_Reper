65cd948f6060a3ca45278a66a77332f706eac08d
jmlc643 <josemarialuyocampos@gmail.com>
2025-05-13 13:43:20 -0500

    Ahora si archivos listos para el despliegue

--- rxconfig.py
+++ rxconfig.py
@@ -9,13 +9,5 @@ class AppConfig(rx.Config):
         "desktop": "1024px",
     }
     backend_port: int = 8000
-    api_url: str = "https://92eed86c-d28d-4ac0-8041-8f06ec9be746.fly.dev"
-    backend_host: str = "92eed86c-d28d-4ac0-8041-8f06ec9be746.fly.dev"
-    cors_allowed_origins: list = [
-        "https://open-reper-cyan-grass.reflex.run",
-        "wss://open-reper-cyan-grass.reflex.run",
-        "http://localhost:3000"
-    ]
-    cors_credentials: bool = True
 
 config = AppConfig()
