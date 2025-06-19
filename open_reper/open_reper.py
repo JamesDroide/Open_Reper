0966cd7f6e8df63d73503fa7184edd38586c7e45
jmlc643 <josemarialuyocampos@gmail.com>
2025-06-19 04:20:02 -0500

    fix(Chess): Se arreglo el error de no mostrarse los peones

--- open_reper/open_reper.py
+++ open_reper/open_reper.py
@@ -1348,13 +1348,13 @@ PIECE_MAP = {
     'b': rx.image(src="/game_pieces/bB.png", width="50px", height="50px"),
     'q': rx.image(src="/game_pieces/bQ.png", width="50px", height="50px"),
     'k': rx.image(src="/game_pieces/bK.png", width="50px", height="50px"),
-    'p': rx.image(src="/game_pieces/bP.png", width="50px", height="50px"),
+    'p': rx.image(src="/game_pieces/bp.png", width="50px", height="50px"),
     'R': rx.image(src="/game_pieces/wR.png", width="50px", height="50px"),
     'N': rx.image(src="/game_pieces/wN.png", width="50px", height="50px"),
     'B': rx.image(src="/game_pieces/wB.png", width="50px", height="50px"),
     'Q': rx.image(src="/game_pieces/wQ.png", width="50px", height="50px"),
     'K': rx.image(src="/game_pieces/wK.png", width="50px", height="50px"),
-    'P': rx.image(src="/game_pieces/wP.png", width="50px", height="50px"),
+    'P': rx.image(src="/game_pieces/wp.png", width="50px", height="50px"),
 }
 
 @rx.page(route="/opening-recommended")
